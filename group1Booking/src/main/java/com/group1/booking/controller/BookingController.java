aa2a1e1e40ef56386c94ec4d3683f8873d3a38f3
Cenon Plana <nonieplana@gmail.com>
2017-09-04 21:37:08 +0800

    project base

---
+++ group1Booking/src/main/java/com/group1/booking/controller/BookingController.java
@@ -0,0 +1,46 @@
+package com.group1.booking.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
+
+//import javax.servlet.http.HttpServletRequest;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BookingController {
+	ObjectMapper jsonMapper = new ObjectMapper();
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(Locale locale, Model model) {
+		ModelAndView modelAndView = new ModelAndView("/js/sample/sample.html");
+		return modelAndView;
+	}
+
+	/*
+	 * return as json if returned value is object. if only one line of string return
+	 * use string
+	 */
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public @ResponseBody String post(@RequestBody String test)
+			throws JsonParseException, JsonMappingException, IOException {
+		return "hello";
+	}
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public @ResponseBody String getAccounts() throws JsonParseException, JsonMappingException, IOException {
+//		return jsonMapper.writeValueAsString(accounts);
+		return "hello";
+	}
+}
